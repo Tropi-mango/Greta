4fd4553793f52c530af383905cccb17345c53f16
Greutz <gorton83@gmail.com>
2020-02-28 16:18:18 +0100

    added name/pics

--- js/contact-carousel.js
+++ js/contact-carousel.js
@@ -19,8 +19,31 @@ $(document).ready(function() {
     firstname = $('#firstname').val();
     subject = $('#subject').val();
     comment = $('#comment').val();
+    switch (firstname[0]){
+      case "a" : 
+        img = "../img/a.jpg";
+        break;
+      case "b" : 
+        img = "../img/b.jpg";
+        break;
+      case "c" : 
+        img = "../img/c.jpg";
+        break;
+      case "d" : 
+        img = "../img/d.jpg";
+        break;
+      case "e" : 
+        img = "../img/e.jpg";
+        break;
+      case "f" : 
+        img = "../img/f.jpg";
+        break;
+      default :
+        img = "../img/h.jpg";
+        break;
+    }
 
-    $('#js-response-comment').append('<div class="card card-inner"><div class="card-body"><div class="row"><div class="col-md-2"><img src="../img/c.jpg" class="img img-rounded img-fluid" style="border-radius: 100%;"><p class="text-secondary text-center">15 Minutes Ago</p></div><div class="col-md-10"><p><a href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>'+firstname+'</strong></a></p><p>'+comment+'</p><p><a class="float-right btn btn-outline-primary ml-2"><i class="fa fa-reply"></i> Reply</a><a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a></p></div></div></div></div>');
+    $('#js-response-comment').append('<div class="card card-inner"><div class="card-body"><div class="row"><div class="col-md-2"><img src="'+img+'" class="img img-rounded img-fluid" style="border-radius: 100%;"><p class="text-secondary text-center">15 Minutes Ago</p></div><div class="col-md-10"><p><a href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>'+firstname+'</strong></a></p><p>'+comment+'</p><p><a class="float-right btn btn-outline-primary ml-2"><i class="fa fa-reply"></i> Reply</a><a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a></p></div></div></div></div>');
 
 
   });
